193a19fe86415c4d71117cbf68b3dd5a9ca6fed6
panpan <patrick.lamatiere@gmail.com>
2019-01-24 22:43:07 +0100

    indent

--- reccorder.py
+++ reccorder.py
@@ -3,25 +3,26 @@ import wave
 import threading
 from multiprocessing import Process, Queue, TimeoutError
 
-form_1 = pyaudio.paInt16 # 16-bit resolution
-chans = 1 # 1 channel
-samp_rate = 44100 # 44.1kHz sampling rate
-chunk = 4096 # 2^12 samples for buffer
-record_secs = 3 # seconds to record
-dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'test1.wav' # name of .wav file
+form_1 = pyaudio.paInt16  # 16-bit resolution
+chans = 1  # 1 channel
+samp_rate = 44100  # 44.1kHz sampling rate
+chunk = 4096  # 2^12 samples for buffer
+record_secs = 3  # seconds to record
+dev_index = 2  # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav'  # name of .wav file
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
+audio = pyaudio.PyAudio()  # create pyaudio instantiation
 
 streamChar = "a"
 
-
 # create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans,
-                    input_device_index = dev_index,input = True,
+stream = audio.open(format=form_1, rate=samp_rate, channels=chans,
+                    input_device_index=dev_index, input=True,
                     frames_per_buffer=chunk)
 
 frames = []
+
+
 def reccord():
     # if streamBool:
 
@@ -40,12 +41,13 @@ def reccord():
             audio.terminate()
 
             # save the audio frames as .wav file
-            wavefile = wave.open(wav_output_filename,'wb')
+            wavefile = wave.open(wav_output_filename, 'wb')
             wavefile.setnchannels(chans)
             wavefile.setsampwidth(audio.get_sample_size(form_1))
             wavefile.setframerate(samp_rate)
             wavefile.writeframes(b''.join(frames))
             wavefile.close()
+
 
 def keyboardInput():
     while True:
@@ -55,12 +57,11 @@ def keyboardInput():
         elif inputVar == "S":
             streamChar = False
 
-if __name__ == '__main__':
-    worker_reccord = Process(target=reccord())
-    worker_keyboardInput = Process(target=keyboardInput())
-
-    worker_keyboardInput.start()
-    worker_reccord.start()
+    # if __name__ == '__main__':
+worker_reccord = Process(target=reccord())
+worker_keyboardInput = Process(target=keyboardInput())
+worker_keyboardInput.start()
+worker_reccord.start()
 
     # worker_qui_met_a_jour_les_flux = Process(target=mettre_a_jour_les_flux,
     #                                          args=(queue_de_flux_a_mettre_a_jour,
