3533e42f0aeca55d054c07fcf6ed94b1cc143343
panpan <patrick.lamatiere@gmail.com>
2019-01-24 22:09:40 +0100

    go depart

--- reccorder.py
+++ reccorder.py
@@ -1,79 +1,39 @@
-from pynput from keyboard
-import time
 import pyaudio
 import wave
 
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz sampling rate
+chunk = 4096 # 2^12 samples for buffer
+record_secs = 3 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav' # name of .wav file
 
-form_1 = pyaudio.paInt16  # 16-bit resolution
-chans = 1  # 1 channel
-samp_rate = 44100  # 44.1kHz sampling rate
-chunk = 4096  # 2^12 sample for buffer
-record_secs = 3  # sec to reccord
-dev_index = 2  # device index found by p.get_device_info_by_index (2 pour l'instant)
-wav_output_filename = 'test1.wav'  # output's file name
+audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-audio = pyaudio.PyAudio()  # audio obj from PyAudio class
-frame = []
+# create pyaudio stream
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+print("recording")
+frames = []
 
-def callback(in_data, frame_count, time_info, status):
-    return (in_data, pyaudio.paContinue)
+# loop through stream and append audio chunks to frame array
+for ii in range(0,int((samp_rate/chunk)*record_secs)):
+    data = stream.read(chunk)
+    frames.append(data)
 
-class MyListener(keyboard.Listener):
-    def __init__(self):
-        super(MyListener, self).__init__(self.on_press, self.on_release)
-        self.key_pressed = None
+print("finished recording")
 
-        self.stream = p.open(format=form_1,
-                             channels = chans,
-                             rate = samp_rate,
-                             input = True,
-                             frame_per_buffer = chunk,
-                             stream_callback = self.callback)
-        print self.stream.is_active()
+# stop the stream, close it, and terminate the pyaudio instantiation
+stream.stop_stream()
+stream.close()
+audio.terminate()
 
-    def on_press(self, key):
-        if key == keyboard.Key.cmd_l:
-            self.key_pressed = True
-
-    def on_release(self, key):
-        if key == keyboard.Key.cmd_l:
-            self.key_pressed = False
-
-    def callback(self, in_data, frame_count, time_info, status):
-        if self.key_pressed == True:
-            return (in_data, pyaudio.paContinue)
-        elif self.key_pressed == False:
-            return (in_data, pyaudio.paComplete)
-        else:
-            return (in_data, pyaudio.paAbort)
-
-listener = MyListener()
-listener.start()
-started = False
-
-while True:
-    time.sleep(0.1)
-    if listener.key_pressed == True and started == False:
-        started = True
-        listener.stream.start_stream()
-        print("stream start")
-
-    elif listener.key_pressed == False and started == True:
-        print("bad")
-        listener.stream.stop_stream()
-        listener.stream.close()
-        p.terminate()
-
-        wf = wave.open(wav_output_filename, 'wb')
-        wf.setnchannels(chans)
-        wf.setsampwidth(p.get_sample_size(form_1))
-        wf.setframerate(samp_rate)
-        wf.writeframes(b''.join(frame))
-        wf.close()
-
-        started = False
-
-# stream = audio.open(format=form_1, rate=samp_rate, channels=chans, input_device_index=dev_index, input=True,
-#                     frames_per_buffer=chunk, stream_callback = self.callback)
-
-
+# save the audio frames as .wav file
+wavefile = wave.open(wav_output_filename,'wb')
+wavefile.setnchannels(chans)
+wavefile.setsampwidth(audio.get_sample_size(form_1))
+wavefile.setframerate(samp_rate)
+wavefile.writeframes(b''.join(frames))
+wavefile.close()
