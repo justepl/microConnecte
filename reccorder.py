38ccd93d9e3bdab0dc5560a8d7e7a2c6512eca4c
panpan <patrick.lamatiere@gmail.com>
2019-01-25 00:43:10 +0100

    try python 2

--- reccorder.py
+++ reccorder.py
@@ -70,7 +70,7 @@ class Reccorder():
             # try:
             print("try")
             print("R for reccord S for Stop \n")
-            inputVar = eval(input())
+            inputVar = raw_input()
             while inputVar != "R" or inputVar != "S":
                 inputVar = eval(input())
 
