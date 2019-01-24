a28b4f4e6a299f3eed7325dfd19f200416b416d7
panpan <patrick.lamatiere@gmail.com>
2019-01-24 22:20:55 +0100

    thread

--- reccorder.py
+++ reccorder.py
@@ -1,5 +1,6 @@
 import pyaudio
 import wave
+import thread
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
@@ -11,29 +12,54 @@ wav_output_filename = 'test1.wav' # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
 
+streamBool = False
+
+
 # create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                    input_device_index = dev_index,input = True, \
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans,
+                    input_device_index = dev_index,input = True,
                     frames_per_buffer=chunk)
-print("recording")
-frames = []
 
-# loop through stream and append audio chunks to frame array
-for ii in range(0,int((samp_rate/chunk)*record_secs)):
-    data = stream.read(chunk)
-    frames.append(data)
+def reccord():
+    if streamBool:
+        frames = []
 
-print("finished recording")
 
-# stop the stream, close it, and terminate the pyaudio instantiation
-stream.stop_stream()
-stream.close()
-audio.terminate()
+        # loop through stream and append audio chunks to frame array
+        for ii in range(0,int((samp_rate/chunk)*record_secs)):
+            data = stream.read(chunk)
+            frames.append(data)
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+    else:
+        print("finished recording")
+
+        # stop the stream, close it, and terminate the pyaudio instantiation
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        # save the audio frames as .wav file
+        wavefile = wave.open(wav_output_filename,'wb')
+        wavefile.setnchannels(chans)
+        wavefile.setsampwidth(audio.get_sample_size(form_1))
+        wavefile.setframerate(samp_rate)
+        wavefile.writeframes(b''.join(frames))
+        wavefile.close()
+
+def keyboardInput():
+    while True:
+        inputVar = input("R for reccord")
+        if inputVar == "R":
+            streamBool = True
+        elif inputVar == "S":
+            streamBool = False
+
+
+try:
+    thread.start_new_thread(reccord())
+    thread.start_new_thread(keyboardInput())
+except:
+    print("Error: unable to start thread")
+
+while 1:
+    pass
