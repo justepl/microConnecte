dd969a4a163a36914cf823c144b00d250d4c347f
panpan <patrick.lamatiere@gmail.com>
2019-01-25 00:08:12 +0100

    delete while true

--- reccorder.py
+++ reccorder.py
@@ -5,7 +5,6 @@ from multiprocessing import Process, Queue, TimeoutError
 import sys
 import time
 
-
 form_1 = pyaudio.paInt16  # 16-bit resolution
 chans = 1  # 1 channel
 samp_rate = 44100  # 44.1kHz sampling rate
@@ -32,54 +31,55 @@ class Reccorder():
     def reccord(self):
         # if streamBool:
         print("func Reccord")
-        while True:
-            streamChar = continuOrNot.get()
-            if streamChar == "R":
-                # loop through stream and append audio chunks to frame array
-                print("reccord")
-                data = stream.read(chunk)
-                frames.append(data)
+        # while True:
+        streamChar = continuOrNot.get()
+        if streamChar == "R":
+            # loop through stream and append audio chunks to frame array
+            print("reccord")
+            data = stream.read(chunk)
+            frames.append(data)
 
-            elif streamChar == "S":
-                print("finished recording")
+        elif streamChar == "S":
+            print("finished recording")
 
-                # stop the stream, close it, and terminate the pyaudio instantiation
-                stream.stop_stream()
-                stream.close()
-                audio.terminate()
+            # stop the stream, close it, and terminate the pyaudio instantiation
+            stream.stop_stream()
+            stream.close()
+            audio.terminate()
 
-                # save the audio frames as .wav file
-                wavefile = wave.open(wav_output_filename, 'wb')
-                wavefile.setnchannels(chans)
-                wavefile.setsampwidth(audio.get_sample_size(form_1))
-                wavefile.setframerate(samp_rate)
-                wavefile.writeframes(b''.join(frames))
-                wavefile.close()
+            # save the audio frames as .wav file
+            wavefile = wave.open(wav_output_filename, 'wb')
+            wavefile.setnchannels(chans)
+            wavefile.setsampwidth(audio.get_sample_size(form_1))
+            wavefile.setframerate(samp_rate)
+            wavefile.writeframes(b''.join(frames))
+            wavefile.close()
 
-    def keyboardInput(self):
-        something = True
-        inputVar = "a"
-        while inputVar != "R" or inputVar != "S":
-            time.sleep(3)
-            try:
-                inputVar = input("R for reccord S for Stop \n")
-                # inputVar.rstrip()
 
-                continuOrNot.put(inputVar)
-                print("queu : ",continuOrNot)
-                inputVar = "a"
-            except EOFError:
-                something = True
+def keyboardInput(self):
+    something = True
+    inputVar = "a"
+    while inputVar != "R" or inputVar != "S":
+        time.sleep(3)
+        try:
+            inputVar = input("R for reccord S for Stop \n")
+            # inputVar.rstrip()
 
-    # except:
-    #     inputVar = "a"
+            continuOrNot.put(inputVar)
+            print("queu : ", continuOrNot)
+            inputVar = "a"
+        except EOFError:
+            something = True
+
+# except:
+#     inputVar = "a"
 
 
 if __name__ == "__main__":
     reccorderInstance = Reccorder()
 
-    worker2 = Process(target=reccorderInstance.keyboardInput, args=())
     worker1 = Process(target=reccorderInstance.reccord, args=())
+    worker2 = Process(target=reccorderInstance.keyboardInput, args=())
 
     worker2.start()
     worker1.start()
